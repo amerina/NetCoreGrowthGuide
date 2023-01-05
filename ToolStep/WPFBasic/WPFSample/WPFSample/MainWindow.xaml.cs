f3aa9d787a5eef02484e699d497d330172b56fc7
wzyandi <wzyandi@gmail.com>
2023-01-05 13:46:58 +0800

    add WPF Sample

---
+++ ToolStep/WPFBasic/WPFSample/WPFSample/MainWindow.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPFSample
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            //DataContext是一个特殊的属性，它将流经可视化树的所有元素。
+            //将它设置在MainWindow上，它将对所有控件可用
+            DataContext = new Model();
+        }
+    }
+}
---
+++ ToolStep/WPFBasic/WPFSample/WPFSample/Step/Model.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace WPFSample
+{
+    /// <summary>
+    /// 大部分过程代码都在模型中
+    /// UI只是声明性的XAML，它通过绑定与模型同步。这就是MVVM的本质。
+    /// </summary>
+    public class Model : INotifyPropertyChanged
+    {
+        public ICommand AddCommand { get; private set; }
+        public Model()
+        {
+            AddCommand = new AddNameCommand(this);
+        }
+        public string CurrentName
+        {
+            get { return mCurrentName; }
+            set
+            {
+                if (value == mCurrentName)
+                    return;
+                mCurrentName = value;
+                OnPropertyChanged();
+            }
+        }
+        string mCurrentName;
+
+        public ObservableCollection<string> AddedNames
+        { get; } = new ObservableCollection<string>();
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        class AddNameCommand : ICommand
+        {
+            Model parent;
+
+            public AddNameCommand(Model parent)
+            {
+                this.parent = parent;
+                parent.PropertyChanged += delegate { CanExecuteChanged?.Invoke(this, EventArgs.Empty); };
+            }
+
+            public event EventHandler CanExecuteChanged;
+
+            public bool CanExecute(object parameter) { return !string.IsNullOrEmpty(parent.CurrentName); }
+
+            public void Execute(object parameter)
+            {
+                parent.AddedNames.Add(parent.CurrentName); ;
+                parent.CurrentName = null;
+            }
+        }
+    }
+}
